1da5371ce99b1c926f864039f0bea334c4cb31ea
Aayuka Nirawade <144752818+nirawadea@users.noreply.github.com>
2025-11-17 22:22:17 -0500

    Time: 4 ms (78.51%), Space: 44.2 MB (63.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,45 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        HashMap<Character, Integer> map = new HashMap<>();
+        // HashMap<Character, Integer> map = new HashMap<>();
 
-        for(char ch : s.toCharArray()){
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // for(char ch : s.toCharArray()){
+        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // }
+
+        // for(char c : t.toCharArray()){
+        //     if(!map.containsKey(c)){
+        //         return false;
+        //     }
+        //     map.put(c, map.getOrDefault(c, 0) - 1);
+        //     if(map.get(c) == 0){
+        //         map.remove(c);
+        //     }
+        // }
+        // return map.isEmpty();
+
+        if(s.length() != t.length()){
+            return false;
         }
 
-        for(char c : t.toCharArray()){
-            if(!map.containsKey(c)){
+        int[] arr = new int[128];
+
+        for(int i=0; i<s.length(); i++){
+            int ascii = s.charAt(i);
+            arr[ascii] = arr[ascii] + 1;
+        }
+
+        for(int i=0; i<t.length(); i++){
+            int ascii = t.charAt(i);
+            arr[ascii] = arr[ascii] - 1;
+        }
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] > 0){
                 return false;
             }
-            map.put(c, map.getOrDefault(c, 0) - 1);
-            if(map.get(c) == 0){
-                map.remove(c);
-            }
         }
-        return map.isEmpty();
+
+        return true;
     }
 }
